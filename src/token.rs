4dd1b29e27a73dd80662abad85a58a15100dc515
Kuz <kuzeygokk@outlook.com>
2026-06-11 22:42:46 -0700

    refactor: improve architecture and handle multi token input

---
+++ src/token.rs
@@ -0,0 +1,65 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+// Alternatively: #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
+pub enum TokenType {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    Equal,
+    EqualEqual,
+    Eof,
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let name = match self {
+            TokenType::LeftParen => "LEFT_PAREN",
+            TokenType::RightParen => "RIGHT_PAREN",
+            TokenType::LeftBrace => "LEFT_BRACE",
+            TokenType::RightBrace => "RIGHT_BRACE",
+            TokenType::Comma => "COMMA",
+            TokenType::Dot => "DOT",
+            TokenType::Minus => "MINUS",
+            TokenType::Plus => "PLUS",
+            TokenType::Semicolon => "SEMICOLON",
+            TokenType::Slash => "SLASH",
+            TokenType::Star => "STAR",
+            TokenType::Equal => "EQUAL",
+            TokenType::EqualEqual => "EQUAL_EQUAL",
+            TokenType::Eof => "EOF",
+        };
+        write!(f, "{name}")
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
+    pub line: usize,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, line: usize) -> Self {
+        Token {
+            token_type,
+            lexeme,
+            line,
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} null", self.token_type, self.lexeme)
+    }
+}
